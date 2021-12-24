498a6e373c20cd24bfbe3bdc1067bdf39e7817c3
harmyderoman <romangarmider@gmail.com>
2021-12-24 11:49:45 +0200

    useDialogWrapper fix

--- src/useDialogWrapper.ts
+++ src/useDialogWrapper.ts
@@ -1,30 +1,14 @@
-import { Component, DefineComponent, h, reactive, Ref, ref, shallowRef, unref } from 'vue-demi';
+import { Component, Ref, shallowRef } from 'vue-demi'
 
 const DialogsStore: Ref<Component[]> = shallowRef([])
 
 export const useDialogWrapper = function () {
-
   const addComponent = function (comp: any) {
     DialogsStore.value.push(comp)
-  }
-
-  const createRenderFunction = function (comp: DefineComponent, show: Ref, confirm: any, cancel: any) {
-
-    return function () {
-      return h(
-        comp,
-        {
-          show: unref(show),
-          onConfirm: confirm,
-          onCancel: cancel
-        },
-      )
-    }
   }
 
   return {
     DialogsStore,
     addComponent,
-    createRenderFunction
   }
-}  
+}
