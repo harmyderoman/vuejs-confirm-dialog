22f3e8fd35bb6bf7f05507dd7c75b0aa3082c1e3
harmyderoman <romangarmider@gmail.com>
2021-12-24 11:49:57 +0200

    test clear

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,8 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { shallowMount } from '@vue/test-utils'
+import DialogsWrapper from '../../src/DialogsWrapper.vue'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+describe('DialogsWrapper.vue', () => {
+  it('testable', () => {
+    expect(true).toBe(true)
+  })
+})
