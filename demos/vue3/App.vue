001b069c1818524f61040588a3d5b89a2f84c38d
harmyderoman <romangarmider@gmail.com>
2021-12-24 11:43:32 +0200

    vue 3 demo

--- demos/vue3/App.vue
+++ demos/vue3/App.vue
@@ -1,26 +1,39 @@
-<template>
-	<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-</template>
+<script setup>
+import ModalWindow from './ModalWindow.vue'
 
-<script lang="ts">
-	import { defineComponent } from 'vue'
-	import HelloWorld from './components/HelloWorld.vue'
+import { create } from '../../src/index'
+import DialogsWrapper from '../../src/DialogsWrapper.vue'
+// import { reactive, shallowRef } from 'vue'
 
-	export default defineComponent({
-		name: 'App',
-		components: {
-			HelloWorld,
-		},
-	})
+const { reveal, onConfirm, onCancel } = create(ModalWindow)
+const { reveal: showDialog2 } = create(ModalWindow, {
+  msg: 'Modal N2',
+})
+
+onConfirm(() => {
+  console.log('Confirmed!')
+})
+onCancel(() => {
+  console.log('Canceled!')
+})
+const showDialog = async () => {
+  const { data, isCanceled } = await reveal({ msg: 'New Msg!' })
+  if (!isCanceled) console.log('Confirmed!')
+  else console.log('Canceled!')
+}
 </script>
 
+<template>
+  <h1>Hello</h1>
+  <button @click="showDialog">Dialog</button>
+  <button @click="showDialog2">Dialog 2</button>
+  <DialogsWrapper />
+</template>
+
 <style lang="scss">
-	#app {
-		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>
---
+++ demos/vue3/ModalWindow.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, defineProps, defineEmits } from 'vue'
+
+defineProps({
+  show: Boolean,
+  msg: {
+    type: String,
+    default: 'Modal Dialog',
+  },
+})
+
+const emit = defineEmits(['confirm', 'cancel'])
+
+// const msg = ref('Modal Dialog')
+</script>
+
+<template>
+  <div v-if="show">
+    <h1>{{ msg }}</h1>
+
+    <button @click="emit('confirm', true)">Confirm</button><br />
+    <button @click="emit('cancel', false)">Cancel</button>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
