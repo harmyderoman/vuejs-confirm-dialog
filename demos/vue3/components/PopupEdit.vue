cbec0b11ae35b9d40720017dbc0d2fe126a6e622
harmyderoman <romangarmider@gmail.com>
2022-09-18 22:23:19 +0300

    PopupEdit: props types

--- demos/vue3/components/PopupEdit.vue
+++ demos/vue3/components/PopupEdit.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
-import { onMounted, ref, PropType} from 'vue'
+import { onMounted, ref, Ref} from 'vue'
 
-const { value, event } = defineProps({
-  value: {
-    type: String,
-    required: true
-  },
-  event: {
-    type: Object as PropType<PointerEvent>,
-    required: true
-  }
-})
+const { value, event } = defineProps<{
+    value:  string | number,
+    event:  PointerEvent
+  }>()
 
-const model = ref('')
+const model: Ref<string | number| null> = ref(null)
 
 onMounted(() => {
   model.value = value
@@ -23,9 +17,10 @@ const emit = defineEmits(['confirm', 'cancel'])
 
 <template>
   <div class="my-modal" @click.self="emit('cancel')">
-    <div class="popup-box" 
-      :style="{ 'top': event.clientY + 'px', 'left': event.clientX + 'px' }"
-      >
+    <div
+      class="popup-box"
+      :style="{ top: event.clientY + 'px', left: event.clientX + 'px' }"
+    >
       <div class="popup-action">
         <input
           type="text"
@@ -44,25 +39,28 @@ const emit = defineEmits(['confirm', 'cancel'])
 <style>
 .popup-box {
   position: absolute;
-    --tw-bg-opacity: 1;
-    background-color: hsla(var(--b1) / var(--tw-bg-opacity, 1));
-    padding: 15px 10px;
-    width: 300px;
-    transform: translateX(var(--tw-translate-x)) translateY(var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
-    transition-property: background-color, border-color, color, fill, stroke, opacity, box-shadow, transform;
-    transition-duration: .15s;
-    transition-duration: .2s;
-    transition-timing-function: cubic-bezier(.4, 0, .2, 1);
-    border-radius: var(--rounded-box, 1rem);
-    box-shadow: 0 25px 50px -12px rgb(0 0 0 / 25%);
+  --tw-bg-opacity: 1;
+  background-color: hsla(var(--b1) / var(--tw-bg-opacity, 1));
+  padding: 15px 10px;
+  width: 300px;
+  transform: translateX(var(--tw-translate-x)) translateY(var(--tw-translate-y))
+    rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y))
+    scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+  transition-property: background-color, border-color, color, fill, stroke,
+    opacity, box-shadow, transform;
+  transition-duration: 0.15s;
+  transition-duration: 0.2s;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  border-radius: var(--rounded-box, 1rem);
+  box-shadow: 0 25px 50px -12px rgb(0 0 0 / 25%);
 }
 
 .popup-action {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
 
 .table th:first-child {
-    z-index: 0;
+  z-index: 0;
 }
 </style>
